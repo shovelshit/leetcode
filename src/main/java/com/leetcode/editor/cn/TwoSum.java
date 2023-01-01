4bc74864f00f20e82eb151df42620c7e9ba60a88
lijinfeng <728729140@qq.com>
2023-01-01 15:02:30 +0800

    两数之和: 	执行耗时:1 ms,击败了99.20% 的Java用户 	内存消耗:41.8 MB,击败了26.27% 的Java用户

---
+++ src/main/java/com/leetcode/editor/cn/TwoSum.java
@@ -0,0 +1,91 @@
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 只会存在一个有效答案 
+// 
+//
+// 
+//
+// 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？ 
+//
+// Related Topics 数组 哈希表 👍 16033 👎 0
+
+
+package com.leetcode.editor.cn;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+        int[] ints = solution.twoSum(new int[]{3,2,4}, 6);
+        System.out.println("ints = " + Arrays.toString(ints));
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+//            int[] temp = new int[2];
+//            for (int i = 0; i < nums.length; i++) {
+//                for (int j = i + 1; j < nums.length; j++) {
+//                    if (nums[i] + nums[j] == target) {
+//                        temp[0] = i;
+//                        temp[1] = j;
+//                        return temp;
+//                    }
+//                }
+//            }
+//            return temp;
+
+            HashMap<Integer, Integer> temp = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int firstValue = target - nums[i];
+                if (temp.containsKey(firstValue)) {
+                    return new int[]{temp.get(firstValue), i};
+                }
+                temp.put(nums[i], i);
+            }
+            return new int[]{};
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
