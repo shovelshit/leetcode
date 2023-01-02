ccd8132a02ca7f416865f5bea7d3b75168f7b4c5
lijinfeng <728729140@qq.com>
2023-01-02 16:31:56 +0800

    合并两个有序链表

---
+++ src/main/java/com/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -0,0 +1,104 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+//
+// Related Topics 递归 链表 👍 2846 👎 0
+
+
+package com.leetcode.editor.cn;
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+
+        ListNode listNode2 = new ListNode(1);
+        listNode2.next = new ListNode(2);
+        listNode2.next.next = new ListNode(3);
+
+        ListNode listNode1 = solution.mergeTwoLists(listNode, listNode2);
+        System.out.println("listNode1 = " + listNode1);
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     */
+//    static class ListNode {
+//        Integer val;
+//        ListNode next;
+//
+//        ListNode() {
+//        }
+//
+//        ListNode(int val) {
+//            this.val = val;
+//        }
+//
+//        ListNode(int val, ListNode next) {
+//            this.val = val;
+//            this.next = next;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "ListNode{" +
+//                    "val=" + val +
+//                    ", next=" + next +
+//                    '}';
+//        }
+//    }
+
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null) {
+                return list2;
+            } else if (list2== null) {
+                return list1;
+            } else if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list2.next, list1);
+                return list2;
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
